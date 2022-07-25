1b3f088a43f421d99a884c0d7e90bd098612ea69
mukesh <mukeshmande@gmail.com>
2022-07-25 11:45:48 +0530

    maven build issue in jenkins fix

--- src/main/java/com/news/search/NewsSearchServiceApplication.java
+++ src/main/java/com/news/search/NewsSearchServiceApplication.java
@@ -1,12 +1,9 @@
 package com.news.search;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class NewsSearchServiceApplication {
 
 	public static void main(String[] args) {
