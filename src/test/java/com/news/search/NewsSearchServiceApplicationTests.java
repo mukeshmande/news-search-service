7b8912d81097a7a36575e8850c4e6be7b2115b02
mukesh <mukeshmande@gmail.com>
2022-07-27 23:22:41 +0530

    test case updated

--- src/test/java/com/news/search/NewsSearchServiceApplicationTests.java
+++ src/test/java/com/news/search/NewsSearchServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.news.search;
 
+import com.news.search.controllers.NewsSearchController;
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class NewsSearchServiceApplicationTests {
 
+	@Autowired
+	NewsSearchController newsSearchController;
+
+
 	@Test
 	void contextLoads() {
+        assertThat(newsSearchController).isNotNull();
 	}
 
 }
